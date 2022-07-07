f48f5ad0feea29dbd25b250f6f330e9c14c7c409
eggpoison <edwrd.magee@gmail.com>
2022-07-07 18:02:25 +1000

    Fix bug with clientID being wrong type

--- src/client-server-types.ts
+++ src/client-server-types.ts
@@ -3,7 +3,7 @@ import Tile from "./Tile";
 export interface ServerToClientEvents {
    terrain: (tiles: Array<Array<Tile>>) => void;
    message: (message: string) => void;
-   chatMessage: (clientID: number, message: string) => void;
+   chatMessage: (clientID: string, message: string) => void;
 }
 
 export interface ClientToServerEvents {
