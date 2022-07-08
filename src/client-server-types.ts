f54b654f646704f65ea9707552fb0c990e62d03c
eggpoison <edwrd.magee@gmail.com>
2022-07-08 16:59:14 +1000

    Add a playerMovement event

--- src/client-server-types.ts
+++ src/client-server-types.ts
@@ -1,22 +1,25 @@
 import Tile from "./Tile";
 
 export type PlayerData = {
+   clientID: string;
    name: string;
    position: [number, number];
 }
+
+export type SocketData = Omit<PlayerData, "clientID">;
 
 export interface ServerToClientEvents {
    terrain: (tiles: Array<Array<Tile>>) => void;
    message: (message: string) => void;
    chatMessage: (senderName: string, message: string) => void;
    newPlayer: (playerData: PlayerData) => void;
-}
+   playerMovement: (clientID: string, movement: number) => void;
+} 
 
 export interface ClientToServerEvents {
    chatMessage: (message: string) => void;
-   playerData: (playerData: PlayerData) => void;
+   playerData: (data: SocketData) => void;
+   playerMovement: (movement: number) => void;
 }
 
 export interface InterServerEvents {}
-
-export interface SocketData extends PlayerData {}
