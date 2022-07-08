8e23ed13fce3509618cb9487c2b912649270f438
eggpoison <edwrd.magee@gmail.com>
2022-07-08 17:02:16 +1000

    Switch the roles of PlayerData and SocketData

--- src/client-server-types.ts
+++ src/client-server-types.ts
@@ -1,24 +1,25 @@
 import Tile from "./Tile";
 
 export type PlayerData = {
-   clientID: string;
    name: string;
    position: [number, number];
 }
 
-export type SocketData = Omit<PlayerData, "clientID">;
+export interface SocketData extends PlayerData {
+   clientID: string;
+}
 
 export interface ServerToClientEvents {
    terrain: (tiles: Array<Array<Tile>>) => void;
    message: (message: string) => void;
    chatMessage: (senderName: string, message: string) => void;
-   newPlayer: (playerData: PlayerData) => void;
+   newPlayer: (playerData: SocketData) => void;
    playerMovement: (clientID: string, movement: number) => void;
 } 
 
 export interface ClientToServerEvents {
    chatMessage: (message: string) => void;
-   playerData: (data: SocketData) => void;
+   playerData: (data: PlayerData) => void;
    playerMovement: (movement: number) => void;
 }
 
