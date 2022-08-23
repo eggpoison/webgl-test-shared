76589b374aee8d3047337350ac9674a85957db89
eggpoison <edwrd.magee@gmail.com>
2022-08-23 12:36:07 +1000

    obama

--- src/client-server-types.ts
+++ src/client-server-types.ts
@@ -1,26 +1,9 @@
 import { Tile } from "./Tile";
 
-export interface SocketData {
-   name: string;
-   position: [number, number];
-   clientID: string;
-}
-
-export type EntityPacket = {
-   readonly positions: Array<[number, number]>;
-   readonly entityID: number;
-}
-
-export type Packet = EntityPacket;
-
 export interface ServerToClientEvents {
-   terrain: (tiles: Array<Array<Tile>>) => void;
    chatMessage: (senderName: string, message: string) => void;
-   newPlayer: (socketData: SocketData) => void;
-   playerMovement: (clientID: string, movementHash: number) => void;
-   position: () => void;
    clientDisconnect: (clientID: string) => void;
-   entityPacket: (packet: EntityPacket) => void;
+   gameData: (gameTicks: number, tiles: Array<Array<Tile>>) => void;
 }
 
 export interface ClientToServerEvents {
