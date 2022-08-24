8a622d4912eb4e11d44be14d01b95585892f3a53
eggpoison <edwrd.magee@gmail.com>
2022-08-24 12:41:06 +1000

    reeeer

--- src/client-server-types.ts
+++ src/client-server-types.ts
@@ -1,20 +1,25 @@
-import { EntityType } from "./entity-info";
+import { EntityInfoClientArgs, EntityType } from "./entity-info";
 import { Tile } from "./Tile";
 
 export type VisibleChunkBounds = [minX: number, maxX: number, minY: number, maxY: number];
 
-export type EntityData = {
+export type EntityData<T extends EntityType> = {
    readonly id: number;
    /** The type of entity (e.g. "cow") */
-   readonly type: EntityType;
+   readonly type: T;
    readonly position: [number, number]; // Point
    readonly velocity: [number, number] | null; // Vector
    readonly acceleration: [number, number] | null; // Vector
    readonly terminalVelocity: number;
+   readonly clientArgs: Parameters<EntityInfoClientArgs[T]>;
 }
 
 export type GameDataPacket = {
-   readonly nearbyEntities: ReadonlyArray<EntityData>;
+   readonly nearbyEntities: ReadonlyArray<EntityData<EntityType>>;
+}
+
+export type PlayerMovementPacket = {
+
 }
 
 // Note to stupid future self: don't remove this, it's important
