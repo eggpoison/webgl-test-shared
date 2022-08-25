9dbbf8168cf0b7abaaaf504d0bedf100f2e075c4
eggpoison <edwrd.magee@gmail.com>
2022-08-26 08:40:04 +1000

    a

--- src/Tile.ts
+++ src/Tile.ts
@@ -1,4 +1,4 @@
-import { Biome } from "./biomes";
+import { BiomeName } from "./biomes";
 
 // excuse the terrible formatting, I'm tired
 export type TileType =
@@ -52,7 +52,7 @@ export const TILE_TYPE_INFO_RECORD: Record<TileType, TileTypeInfo> = {
       friction: 0.5
    },
    snow: {
-      friction: 0.7
+      friction: 0.75
    },
    ice: {
       friction: 0.15
@@ -65,63 +65,15 @@ export const TILE_TYPE_INFO_RECORD: Record<TileType, TileTypeInfo> = {
    }
 };
 
-/*
-[TileType.grass]: {
-      textureSource: "grass.png",
-      friction: DEFAULT_FRICTION
-   },
-   [TileType.dirt]: {
-      textureSource: "dirt.png",
-      friction: DEFAULT_FRICTION
-   },
-   [TileType.water]: {
-      colour: [0, 240, 228],
-      friction: DEFAULT_FRICTION,
-      isLiquid: true
-   },
-   [TileType.sludge]: {
-      textureSource: "sludge.png",
-      friction: DEFAULT_FRICTION
-   },
-   [TileType.rock]: {
-      textureSource: "rock.png",
-      friction: DEFAULT_FRICTION
-   },
-   [TileType.sand]: {
-      textureSource: "sand.png",
-      friction: DEFAULT_FRICTION
-   },
-   [TileType.sandstone]: {
-      textureSource: "sandstone.png",
-      friction: DEFAULT_FRICTION
-   },
-   [TileType.snow]: {
-      textureSource: "snow.png",
-      friction: 0.7
-   },
-   [TileType.ice]: {
-      textureSource: "ice.png",
-      friction: 0.15
-   },
-   [TileType.magma]: {
-      textureSource: "grass.png",
-      friction: DEFAULT_FRICTION
-   },
-   [TileType.lava]: {
-      textureSource: "grass.png",
-      friction: 0.8
-   }
-*/
-
 export interface TileInfo {
    type: TileType;
-   biome: Biome;
+   biome: BiomeName;
    isWall: boolean;
 }
 
 export class Tile implements TileInfo {
    public readonly type: TileType;
-   public readonly biome: Biome;
+   public readonly biome: BiomeName;
    public readonly isWall: boolean;
 
    constructor({ type, biome, isWall }: TileInfo) {
