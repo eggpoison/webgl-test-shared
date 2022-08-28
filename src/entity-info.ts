1c595b115581e088bc9cdfb28ad50573158918c3
eggpoison <edwrd.magee@gmail.com>
2022-08-28 15:31:06 +1000

    a

--- src/entity-info.ts
+++ src/entity-info.ts
@@ -2,9 +2,23 @@ export type EntityBehaviour = "passive" | "neutral" | "hostile";
 
 export type EntityType = "cow" | "player";
 
+export type CircularHitbox = {
+   readonly type: "circular";
+   readonly radius: number;
+}
+
+export type RectangularHitbox = {
+   readonly type: "rectangular";
+   readonly width: number;
+   readonly height: number;
+}
+
+export type Hitbox = CircularHitbox | RectangularHitbox;
+
 type BaseEntityInfo = {
    readonly category: "mob" | "resource" | "other";
    readonly clientArgs?: ReadonlyArray<unknown>;
+   readonly hitbox: Hitbox
 }
 
 interface MobEntityInfo extends BaseEntityInfo {
@@ -25,10 +39,19 @@ export type EntityInfo = MobEntityInfo | ResourceEntityInfo | OtherEntityInfo;
 export const ENTITY_INFO_RECORD: Record<EntityType, EntityInfo> = {
    cow: {
       category: "mob",
-      behaviour: "passive"
+      behaviour: "passive",
+      hitbox: {
+         type: "rectangular",
+         width: 64,
+         height: 128
+      }
    },
    player: {
-      category: "other"
+      category: "other",
+      hitbox: {
+         type: "circular",
+         radius: 32
+      }
    }
 };
 
